4d15809e9d8b407ca1ecb697621972dca4188dc8
fayezzouari <fayez.zouari@insat.ucar.tn>
2024-11-21 09:54:44 +0100

    fixed auth bug

--- src/app/api/logout/route.ts
+++ src/app/api/logout/route.ts
@@ -2,7 +2,8 @@ import { NextResponse } from 'next/server'
 import { cookies } from 'next/headers'
 
 export async function POST() {
-  cookies().delete('auth')
+  const cookieStore = await cookies()
+  cookieStore.delete('auth')
   return NextResponse.json({ success: true })
 }
 
