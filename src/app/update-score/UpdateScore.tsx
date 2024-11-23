abfb5fbe898dc31c4f45536f50fcec254d46b753
fayezzouari <fayez.zouari@insat.ucar.tn>
2024-11-23 23:23:07 +0100

    final

--- src/app/update-score/UpdateScore.tsx
+++ src/app/update-score/UpdateScore.tsx
@@ -49,7 +49,7 @@ export default function UpdateScore() {
     await fetch('/api/sheets', {
       method: 'POST',
       headers: { 'Content-Type': 'application/json' },
-      body: JSON.stringify({ action: 'updateScore', name: updateTeamName, problemName: updateProblemName }),
+      body: JSON.stringify({ action: 'updateScore', teamName: updateTeamName, problemName: updateProblemName }),
     })
     setUpdateTeamName('')
     setUpdateProblemName('')
